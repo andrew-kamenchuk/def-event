ce676e5733b4b4e1c664d891bd22a8ba5a40409c
andrew-kamenchuk <andrew.kamenchuk@gmail.com>
2016-08-28 21:46:12 +0300

    .

--- DispatcherAwareInterface.php
+++ DispatcherAwareInterface.php
@@ -4,7 +4,12 @@ namespace def\Event;
 interface DispatcherAwareInterface
 {
     /**
-     * @return def\Event\Dispatcher;
+     * @return def\Event\Dispatcher
      */
     public function getDispatcher();
+
+    /**
+     * @param def\Event\Dispatcher $dispatcher
+     */
+    public function setDispatcher(Dispatcher $dispatcher);
 }
